fe540cd19a531ac8cd4ab944ff1f9d9479a6e28f
Bejster67 <maciej.bejster@gmail.com>
2025-11-23 12:50:05 +0100

    Delete NotFound component
    Remove NotFound component that displayed a 404 error message.

--- src/src/src/pages/src/pages/src/pages/NotFound.tsx
+++ src/src/src/pages/src/pages/src/pages/NotFound.tsx
@@ -1,7 +1,1 @@
-export default function NotFound() {
-  return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">404 – Page Not Found</h1>
-    </div>
-  );
-}
+
